2a0bf861167a809c7325b37ee3d03025222bda47
Wocked61 <DylanP@csu.fullerton.edu>
2024-12-08 17:16:48 -0800

    made a bot that fills a cart for a user

--- bots.py
+++ bots.py
@@ -1,5 +1,37 @@
-def bot_clerk(items):
-    pass
+import threading
+import time
+import json
 
-def bot_fetcher():
-    pass
+with open("inventory.dat", "r") as f:
+    inventory = json.load(f)
+
+def bot_clerk(items):
+    cart = []
+    lock = threading.Lock()
+    threads = []
+    robot_fetchers = [[], [], []]
+    for i, item in enumerate(items):
+        item_index = i % 3
+        robot_fetchers[item_index].append(item)
+
+
+    for robot_list in robot_fetchers:
+        if robot_list:
+            thread = threading.Thread(target=bot_fetcher, args=(robot_list, cart, lock))
+            threads.append(thread)
+            thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    return cart
+
+def bot_fetcher(item_list, cart, lock):
+    for item in item_list:
+        item_info = inventory[item]
+        item_disc = item_info[0]
+        item_time = item_info[1]
+        time.sleep(item_time)
+
+        with lock:
+            cart.append([item, item_disc])
